e218973500d9012dda85df928d6b591d17297ac5
anonymous <anonymous>
2017-01-31 18:32:56 -0500

    3rd commit: definition of retDec added in /src/roman_ops.c and test case passes

--- src/roman_ops.c
+++ src/roman_ops.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 #include "roman_ops.h"
 
-int main(){
+int retDec(char ch){
 
-	printf("here it starts");
-	return 0;
+    if(ch=='i'||ch=='I') return 1;
+    else if(ch=='v'||ch=='V') return 5;
+    else if(ch=='x'||ch=='X') return 10;
+    else if(ch=='l'||ch=='L') return 50;
+    else if(ch=='c'||ch=='C') return 100;
+    else if(ch=='d'||ch=='D') return 500;
+    else if(ch=='m'||ch=='M') return 1000;
 }
+
+
+
