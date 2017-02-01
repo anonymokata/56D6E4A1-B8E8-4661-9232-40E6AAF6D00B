31dba785813ae8520bc1dd0db046903b905ecde5
anonymous <anonymous>
2017-02-01 15:45:50 -0500

    13th commit: completed driver program (driver.c) and added a README

---
+++ README.txt
@@ -0,0 +1,20 @@
+Author: Al Amin Hosain
+Program Name: Roman Numeral Calculator v1.0.0
+---------------------------------------------------------------------
+
+This program is an implementation of basic Roman Numerals operations.
+Development was crafted using Test Driven Development paradigm.
+Following are some instructions useful to examine functionalities of the program,
+
+
+1. Unit test cases are written using check unit test framework ("tests" directory)
+2. Makefile in tests directory will produce an executable (test_exe) to examine test results
+3. There is also a driver program (driver.c) in top directory
+4. Running Makefile in top directory will create an executable (driver_exe) for driver program in "bin" directory
+5. Running driver_exe would let user to play with the calculator
+6. In case of subtraction operation it is assumed that user first enter first operand and then second operand
+7. Valid roman numerals range is assumed as (1 to 3999) inclusive
+8. It is assumed that user will input roman numerals in valid range
+9. In case an operation produce a roman numeral outside valid range, driver program will inform user and keep continuing
+
+
--- driver.c
+++ driver.c
@@ -1,15 +1,50 @@
 /*
 This is a simple driver program for roman calculator libaray
+This is a menu driven program provides the operability of a roman calculator
+Also provides informative messages on invalid inputs
 */
 
 
 
 #include<stdio.h>
+#include "roman_ops.h"
 
 int main(){
 
+	
+    int choice,num1,num2;
+	char str[ARRAY_SZ],str2[ARRAY_SZ],str3[ARRAY_SZ];
+	while(1){
 
+        printf("\n##########RomanCalculator#########\n");
+        printf("\nSelect Operations\n1.Addition.\n2.Subtraction.\n3.Exit.\n");
+        scanf("%d",&choice);
+        if(choice==3) break;
+        else{
+            printf("Please enter first operand:");scanf("%s",str);
+            printf("Please enter second operand:");scanf("%s",str2);
+            num1 = toDec(str);
+            num2 = toDec(str2);
+            
+			if(num1>3999 || num2>3999)  {printf("\nBAD INPUT - One or both of inputs exceed valid upper limit of roman numerals\n");continue;}
+
+			
+			if(choice==1){
+            	if(num1+num2>3999) {printf("\nBAD INPUT - Result exceeds valid upper limit of roman numerals\n");continue;}
+			    toRoman(str3,num1+num2);
+                printf("Addition result: %s\n",str3);
+            }
+            else if(choice==2){
+                if(num1-num2>=1){
+                    toRoman(str3,num1-num2);
+                    printf("Subtraction result: %s\n",str3);
+                }
+                else printf("\nBAD INPUT - Subtraction not possible!\n");
+            }
+			else continue;
+
+        }
+    }
 	return 0;
 }
-
 
